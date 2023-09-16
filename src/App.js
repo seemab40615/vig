7efce128a60b55d7ba47d4bcd905fba182724369
Talha Nazir <talha@cloudmeshsolutions.com>
2023-09-16 18:16:05 +0500

    font

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 function App() {
   return (
     <div className="bg-black w-maxSize h-screen mx-auto">
-      <ul>
-        <li className="text-white font-gridular">hkl</li>
-      </ul>
+      <div className="text-white flex justify-between">
+        <div className="flex justify-between max-w-[870px] w-full">
+          <ul className="flex gap-x-1">
+            <li className="px-4 py-3 roudned">Home</li>
+            <li className="px-4 py-3">Platform Solutions</li>
+            <li className="px-4 py-3">Resources</li>
+            <li className="px-4 py-3">Contact</li>
+          </ul>
+          <h1>Logo</h1>
+        </div>
+        <h1>WishList</h1>
+      </div>
     </div>
   );
 }
